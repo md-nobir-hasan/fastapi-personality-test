acdec8e5d572bce9f9c74c37a317ba8215474e88
Md. Anower Hossan <anowerhossan1988@gmail.com>
2025-04-14 10:34:30 +0600

    feedback fixed

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -357,6 +357,7 @@ function resetTranslation() {
 function resetTranslation() {
   // Clear the translate cookie (which stores language preference)
   document.cookie = 'googtrans=;path=/;expires=Thu, 01 Jan 1970 00:00:00 UTC';
+  document.cookie = 'googtrans=;path=/;domain=' + window.location.hostname + ';expires=Thu, 01 Jan 1970 00:00:00 UTC';
 
   // Force reload without translation
   window.location.reload();
